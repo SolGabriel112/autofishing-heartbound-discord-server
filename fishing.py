e920a99cd0181cb7c803d0f16dd15a297dffc24b
Gabriel <146453573+SolGabriel112@users.noreply.github.com>
2025-12-27 20:10:09 +0100

    fishing.py

--- fishing.py
+++ fishing.py
@@ -1,31 +1,36 @@
 import pyautogui
 import time
 import ctypes
-
+#the command to run it it case you have multiple python version installed
+"py -3.12 .\fishing.py"
 ctypes.windll.user32.SetProcessDPIAware()
 pyautogui.FAILSAFE = True
 
 print("Waiting for button...")
 pressed = False
 
-while True:
+
+for i in range(200):
     while pressed != True:
         try:
             loc = pyautogui.locateCenterOnScreen("clickme.png", confidence=0.8)
             if loc:
                 print("Button found at:", loc)
-                pyautogui.moveTo(loc, duration=0.3)
+                pyautogui.moveTo(loc, duration=0.1)
+                time.sleep(0.15)
                 pyautogui.click()
                 print("Clicked!")
-                time.sleep(1.25)
+                print("fished",i+1,"times")
+                time.sleep(1.4)
                 pyautogui.scroll(-100)  # scroll down 10 "clicks"
-                time.sleep(0.15)
+                time.sleep(0.17)
                 pyautogui.write(',')
-                time.sleep(0.15)
+                time.sleep(0.17)
                 pyautogui.write('f')
-                time.sleep(0.15)
+                time.sleep(0.17)
                 pyautogui.press('enter')
-                time.sleep(0.6)
+                time.sleep(0.67)
+                pressed = True
                 
             else:
                 print("Button not found, waiting...")
@@ -33,4 +38,4 @@ while True:
         except pyautogui.ImageNotFoundException:
             print("Button not on screen yet...")
             time.sleep(0.5)
-    
+    pressed = False
